38270ef0ec56b2b4cf80554a1767f07d10188520
timothestes <timothestes@gmail.com>
2023-05-03 16:05:58 -0700

    address code review
    add conditional statements for setting python and poetry versions
    
    move fixtures to conftest
    
    switch to poetry.toml and update authors
    
    move try-catch code to separate function
    
    add better class docstrings
    
    add module docstring
    
    remove bad comments
    
    add cache comment
    
    nit: change name of install step
    
    nit: remove unused testing options
    
    compact small commands

--- tests/test_plugin.py
+++ tests/test_plugin.py
@@ -3,53 +3,6 @@ import pytest
 from poetry_partifact_plugin.plugin import PartifactPlugin
 
 
-@pytest.fixture
-def parsed_pyproject_toml_with_non_codeartifact_source() -> dict:
-    return {
-        "tool": {
-            "poetry": {
-                "source": [
-                    {
-                        "url": "https://pypi.org",
-                        "name": "my_source_name",
-                    }
-                ]
-            }
-        }
-    }
-
-
-@pytest.fixture
-def correct_parsed_pyproject_toml() -> dict:
-    return {
-        "tool": {
-            "poetry": {
-                "source": [
-                    {
-                        "url": "https://my-domain-123456789012.d.codeartifact.us-west-2.amazonaws.com/pypi/my-repo/simple/",
-                        "name": "my_source_name",
-                    }
-                ]
-            }
-        }
-    }
-
-
-@pytest.fixture
-def parsed_pyproject_toml_no_name() -> dict:
-    return {
-        "tool": {
-            "poetry": {
-                "source": [
-                    {
-                        "url": "https://my-domain-123456789012.d.codeartifact.us-west-2.amazonaws.com/pypi/my-repo/simple/",
-                    }
-                ]
-            }
-        }
-    }
-
-
 def test_object_initialization():
     """Unsure of the best way to test a poetry plugin..."""
     plugin = PartifactPlugin()
